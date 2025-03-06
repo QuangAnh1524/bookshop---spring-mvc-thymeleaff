8c8cf440417130e1b48d5c325a09e6227ed30cb7
QuangAnh1524 <quanganha6k58@gmail.com>
2025-03-06 09:43:31 +0700

    cart

--- 01 spring mvc/src/main/java/com/quanh1524/bookshop/controller/client/HomePageController.java
+++ 01 spring mvc/src/main/java/com/quanh1524/bookshop/controller/client/HomePageController.java
@@ -1,10 +1,12 @@
 package com.quanh1524.bookshop.controller.client;
 
+import com.quanh1524.bookshop.domain.Cart;
 import com.quanh1524.bookshop.domain.Product;
 import com.quanh1524.bookshop.domain.Role;
 import com.quanh1524.bookshop.domain.User;
 import com.quanh1524.bookshop.domain.dto.RegisterDTO;
 import com.quanh1524.bookshop.repository.UserRepository;
+import com.quanh1524.bookshop.service.CartService;
 import com.quanh1524.bookshop.service.ProductService;
 import com.quanh1524.bookshop.service.SecurityUtil;
 import com.quanh1524.bookshop.service.UserService;
@@ -30,23 +32,32 @@ public class HomePageController {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final SecurityUtil securityUtil;
+    private final CartService cartService;
 
-    public HomePageController(ProductService productService, UserService userService, PasswordEncoder passwordEncoder, UserRepository userRepository, SecurityUtil securityUtil) {
+    public HomePageController(ProductService productService, UserService userService, PasswordEncoder passwordEncoder, UserRepository userRepository, SecurityUtil securityUtil, CartService cartService) {
         this.productService = productService;
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.securityUtil = securityUtil;
+        this.cartService = cartService;
     }
 
     @GetMapping("/")
     public String HomePage(Model model, HttpSession session) {
         List<Product> productList = productService.getAllProducts();
         model.addAttribute("productsFromView", productList);
+
         SecurityUtil.UserInfo userInfo = securityUtil.getCurrentUserInfo();
 //        System.out.printf("Session ID: %s%n", session.getId());
 //        System.out.printf("Create at: %s%n", new Date(session.getCreationTime()));
 //        System.out.println("Session Last Accessed: " + new Date(session.getLastAccessedTime()));
+        int cartItemCount = 0;
+        if (userInfo.isLoggedIn()) {
+            Cart cart = cartService.getOrCreateCart(userService.findByEmail(userInfo.getEmail()).getFirst(), session);
+            cartItemCount = cart.getSum();
+        }
+        model.addAttribute("cartItemCount", cartItemCount);
         model.addAttribute("isLoggedIn", userInfo.isLoggedIn());
         model.addAttribute("userEmail", userInfo.getEmail());
         model.addAttribute("isAdmin", userInfo.isAdmin());
@@ -54,9 +65,17 @@ public class HomePageController {
     }
 
     @GetMapping("/login")
-    public String getLoginPage(Model model, @RequestParam(value = "logout", required = false) String logout) {
+    public String getLoginPage(Model model, @RequestParam(value = "logout", required = false) String logout, HttpSession session) {
         if (logout != null) {
             model.addAttribute("message", "Log out successfully");
+        }
+        //sau khi dang nhap thanh cong, ktra pendingProductId
+        SecurityUtil.UserInfo userInfo = securityUtil.getCurrentUserInfo();
+        if (userInfo.isLoggedIn() && session.getAttribute("pendingProductId") != null) {
+            Long productId = (Long) session.getAttribute("pendingProductId");
+            cartService.addProductToCart(productId, 1, userService.findByEmail(userInfo.getEmail()).getFirst(), session);
+            session.removeAttribute("pendingProductId");
+            return "redirect:/";
         }
         return "client/auth/login";
     }
@@ -86,5 +105,16 @@ public class HomePageController {
         this.userService.handleSaveUser(user);
 
         return "redirect:/login";
+    }
+
+    @GetMapping("/cart")
+    public String getCartPage(Model model, HttpSession session) {
+        SecurityUtil.UserInfo userInfo = securityUtil.getCurrentUserInfo();
+        Cart cart = cartService.getOrCreateCart(userInfo.isLoggedIn() ? userService.findByEmail(userInfo.getEmail()).getFirst() : null, session);
+        model.addAttribute("cart", cart);
+        model.addAttribute("isLoggedIn", userInfo.isLoggedIn());
+        model.addAttribute("userEmail", userInfo.getEmail());
+        model.addAttribute("isAdmin", userInfo.isAdmin());
+        return "client/cart/show";
     }
 }
